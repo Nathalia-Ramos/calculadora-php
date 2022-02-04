01b4968f0ee9141c1a49d20c74fcfb4bc0864ba1
NATHALIA APARECIDA RAMOS <21276387@CFP127ALUNO.LOCAL>
2022-02-04 15:53:02 -0300

    first commit

---
+++ modulo/config.php
@@ -0,0 +1,20 @@
+<?php 
+
+   /*********************************
+    * Objetivo: Arquivo responsável por reunir todas as variaveis  e constantes
+        que serão utilizadas np projeto
+    *Autor: Nathalia
+    *Data: 04/02/2022
+    *Versão 1.0
+    *********************************/ 
+
+    //constantes do projeto 
+    //Modo 01 de criar uma constante 
+    define('ERRO_MSG_CAIXA_VAZIA', '<script> alert("Favor escolher uma operação válida"); </script>');
+
+    //Modo 02 de criar uma constante 
+    const ERRO_MSG_OPERACAO_CALCULO = '<script> alert("Favor preencher todas as caixas!"); </script>';
+    const ERRO_MSG__CARACTER_INVALIDO_TEXTO = '<script> alert("Não é possível realizar calculos de dados não numericos!"; </script>';
+    const ERRO_MSG_DIVISAO_ZERO = '<script> alert("Não é possível realizar divisão, onde o valor 2 é igual a 0!"; </script>';   
+    
+?>
